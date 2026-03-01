0fedb0fc7d2cf3ce71b73baed1c9dd4441c79c7e
Albertus <albertussuryapangestu@gmail.com>
2026-03-01 21:50:07 +0700

    menambahkan login dan dashboard admin

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -9,9 +8,21 @@ app.use(express.static(path.join(__dirname)));
 
 // Default route ke index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+// Route untuk halaman login sarpras
+app.get('/sarpras.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'sarpras.html'));
+});
+
+// Alternatif: route dengan nama yang lebih simple
+app.get('/sarpras', (req, res) => {
+    res.sendFile(path.join(__dirname, 'sarpras.html'));
 });
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`- Login Mahasiswa: http://localhost:${PORT}/`);
+    console.log(`- Login Sarpras: http://localhost:${PORT}/sarpras`);
 });
